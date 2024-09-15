40d18ad7a8d48e956ddc96087100c8eeb3e5546c
faisalAbdo11 <93899814+faisal0coding@users.noreply.github.com>
2024-09-15 15:24:22 +0300

    fixing an error in /profile/info/page.js

--- client/app/profile/info/page.js
+++ client/app/profile/info/page.js
@@ -3,9 +3,9 @@ import React from "react";
 const InputField = ({ text }) => {
     return (
         <div className="m-2">
-            <label for={text} class="block text-sm font-medium leading-6 text-gray-900">{text}</label>
-            <div class="relative mt-2 rounded-md shadow-sm">
-                <input type="text" name={text} id={text} class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <label htmlFor={text} className="block text-sm font-medium leading-6 text-gray-900">{text}</label>
+            <div className="relative mt-2 rounded-md shadow-sm">
+                <input type="text" name={text} id={text} className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
         </div>
     );
